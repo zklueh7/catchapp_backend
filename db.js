4fb8d046394d8fce5dddba70b9296ebdde7da82d
zoey klueh <zklueh@gmail.com>
2024-11-11 14:51:23 -0700

    added debug string to see if remote database connection is working properly

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ const { getDatabaseUri } = require("./config");
 
 let db;
 
+
 if (process.env.NODE_ENV === "production") {
   db = new Client({
     connectionString: getDatabaseUri(),
@@ -18,6 +19,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-db.connect();
+db.connect()
+  .then(() => console.log("Database connected successfully"))
+  .catch(err => console.error("Database connection error", err.stack));;
 
 module.exports = db;
